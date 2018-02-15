43cf23e8820b9544caefb2b645a50eb90595933a
ijanus <emekamicro@gmail.com>
2018-02-15 23:56:39 +0100

    Changed Solidity file From Store.sol to PaymentChannel.sol

--- smartContract/test/mainTest.js
+++ smartContract/test/mainTest.js
@@ -2,10 +2,11 @@ const test = require("blue-tape");
 
 const startCal = require("./startCal.js");
 
-const Stores = artifacts.require("../contracts/Store.sol");
+//const Stores = artifacts.require("./Store.sol");
+const Pays = artifacts.require("./PaymentChannels.sol");
 
 (async () => {
-  const instance = await Stores.deployed();
+  //const instance = await Stores.deployed();
+  const instance = await Pays.deployed();
   startCal(test, instance);
 })();
-
