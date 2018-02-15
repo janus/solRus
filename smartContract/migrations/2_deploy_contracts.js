be5d4d16076fab54dbf0120934858ac202a472ab
ijanus <emekamicro@gmail.com>
2018-02-16 00:03:45 +0100

    Deployed PaymentChannels.sol

--- smartContract/migrations/2_deploy_contracts.js
+++ smartContract/migrations/2_deploy_contracts.js
@@ -1,8 +1,9 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
+//var Store = artifacts.require("./Store.sol");
+var Pays = artifacts.require("./PaymentChannels.sol");
 //var MetaCoin = artifacts.require("./MetaCoin.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
+  deployer.deploy(Pays);
   //deployer.link(ConvertLib, MetaCoin);
   //deployer.deploy(MetaCoin);
 };
