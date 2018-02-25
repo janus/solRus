13330bb0e74a0e3ee8f535c4842ee20c334d09c2
ijanus <emekamicro@gmail.com>
2018-02-25 15:20:35 +0100

    Copied from guac

--- smartContract/migrations/2_deploy_contracts.js
+++ smartContract/migrations/2_deploy_contracts.js
@@ -1,9 +1,9 @@
 //var Store = artifacts.require("./Store.sol");
-var Pays = artifacts.require("./PaymentChannels.sol");
+var PaymentChannels = artifacts.require("./PaymentChannels.sol");
 //var MetaCoin = artifacts.require("./MetaCoin.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Pays);
+  deployer.deploy(PaymentChannels);
   //deployer.link(ConvertLib, MetaCoin);
   //deployer.deploy(MetaCoin);
 };
