bb9f46c7d6c724b00c3eeb48e57ad9c2565b069d
ijanus <emekamicro@gmail.com>
2018-02-26 22:04:07 +0100

    Cleaned up code

--- smartContract/test/closeChannel.js
+++ smartContract/test/closeChannel.js
@@ -22,28 +22,22 @@ const {
 module.exports = async (test, instance) => {
   test("closeChannel happy path no hashlocks", async t => {
     const snapshot = await takeSnapshot();
-    const data = await getData(17);
-    const data1 = await getSettlingData(17);
+    const data = await getData(1);
+    const data1 = await getSettlingData(1);
     const hashlocks = "0x";
 
     await closeChannel(instance, data, data1, hashlocks);
 
-    t.equal(
-      (await instance.balanceOf.call(data.address_0)).toString(),
-      "24000"
-    );
-    t.equal(
-      (await instance.balanceOf.call(data.address_1)).toString(),
-      "20000"
-    );
+    t.equal((await instance.balanceOf.call(data.addr_0)).toString(), "24000");
+    t.equal((await instance.balanceOf.call(data.addr_1)).toString(), "20000");
 
     await revertSnapshot(snapshot);
   });
 
   test("channel does not exist", async t => {
     const snapshot = await takeSnapshot();
-    const data = await getData(6);
-    const data1 = await getSettlingData(6);
+    const data = await getData(1);
+    const data1 = await getSettlingData(1);
 
     await closeChannel(instance, data, data1, "0x");
     await t.shouldFail(instance.closeChannel("0x" + data1.chl_id_wg));
@@ -53,8 +47,8 @@ module.exports = async (test, instance) => {
 
   test("channel is not settled", async t => {
     const snapshot = await takeSnapshot();
-    const data = await getData(6);
-    const data1 = await getSettlingData(6);
+    const data = await getData(1);
+    const data1 = await getSettlingData(1);
 
     await createChannel(instance, data);
     // already existing
@@ -67,8 +61,8 @@ module.exports = async (test, instance) => {
 
   test("channel is already closed", async t => {
     const snapshot = await takeSnapshot();
-    const data = await getData(7);
-    const data1 = await getSettlingData(7);
+    const data = await getData(1);
+    const data1 = await getSettlingData(1);
 
     await closeChannel(instance, data, data1, "0x");
     await t.shouldFail(closeChannelWithoutNewChannel(instance, data1, "0x"));
@@ -78,10 +72,10 @@ module.exports = async (test, instance) => {
 
   test("hashlocks do not match", async t => {
     const snapshot = await takeSnapshot();
-    const data = await getData(0);
-    const data1 = await getSettlingData(0);
+    const data = await getData(1);
+    const data1 = await getSettlingData(1);
 
-    //await createChannel(instance, data);
+    await createChannel(instance, data);
     // already existing
     await t.shouldFail(closeChannel(instance, data, data1, "0x06"));
 
@@ -90,8 +84,8 @@ module.exports = async (test, instance) => {
 
   test("bad amount", async t => {
     const snapshot = await takeSnapshot();
-    const data = await getData(8);
-    const data1 = await getSettlingData(8);
+    const data = await getData(1);
+    const data1 = await getSettlingData(1);
 
     await createChannel(instance, data);
     await t.shouldFail(closeChannelSp(instance, data, data1, "0x"));
@@ -101,8 +95,8 @@ module.exports = async (test, instance) => {
 
   test("closeChannel happy path with hashlocks (1 missing preimage)", async t => {
     const snapshot = await takeSnapshot();
-    const data = await getData(18);
-    const data1 = await getSettlingData(18);
+    const data = await getData(2);
+    const data1 = await getSettlingData(2);
     const data2 = await getSpData(0);
 
     const channelId =
@@ -124,29 +118,36 @@ module.exports = async (test, instance) => {
     const hashlock2 = `${solSha3(preimage2).slice(2)}${toSolInt256(10001)}`;
     const hashlock3 = `${solSha3(preimage3).slice(2)}${toSolInt256(2)}`;
 
+    //console.log(
+    //console(`0x${hashlock1}${hashlock2}${hashlock3}`)
+
+    let mydata = {
+      chl_id: data1.chl_id,
+      seq_num: data1.seq_num,
+      bal_0: data1.bal_0,
+      bal_1: data1.bal_1,
+      sig_0: data2.sig_0,
+      sig_1: data2.sig_1,
+      sig_start_stl_p: data1.sig_start_stl_p
+    };
+
     await closeChannel(
       instance,
       data,
-      data2,
+      mydata,
       `0x${hashlock1}${hashlock2}${hashlock3}`
     );
 
-    t.equal(
-      (await instance.balanceOf.call(data.address_0)).toString(),
-      "23999"
-    );
-    t.equal(
-      (await instance.balanceOf.call(data.address_1)).toString(),
-      "20001"
-    );
+    t.equal((await instance.balanceOf.call(data.addr_0)).toString(), "23999");
+    t.equal((await instance.balanceOf.call(data.addr_1)).toString(), "20001");
 
     await revertSnapshot(snapshot);
   });
 
   test("closeChannel happy path with lots of hashlocks", async t => {
     const snapshot = await takeSnapshot();
-    const data = await getData(19);
-    const data1 = await getSettlingData(19);
+    const data = await getData(3);
+    const data1 = await getSettlingData(3);
     const data2 = await getSpData(1);
 
     let hashlocks = "0x";
@@ -162,48 +163,46 @@ module.exports = async (test, instance) => {
       amount = -amount;
     }
 
+    let mydata = {
+      chl_id: data1.chl_id,
+      seq_num: data1.seq_num,
+      bal_0: data1.bal_0,
+      bal_1: data1.bal_1,
+      sig_0: data2.sig_0,
+      sig_1: data2.sig_1,
+      sig_start_stl_p: data1.sig_start_stl_p
+    };
+
     await instance.submitPreimages(preimages);
 
     await mineBlocks(1);
-    await closeChannel(instance, data, data2, hashlocks);
+    await closeChannel(instance, data, mydata, hashlocks);
 
-    t.equal(
-      (await instance.balanceOf.call(data.address_0)).toString(),
-      "24000"
-    );
-    t.equal(
-      (await instance.balanceOf.call(data.address_1)).toString(),
-      "20000"
-    );
+    t.equal((await instance.balanceOf.call(data.addr_0)).toString(), "24000");
+    t.equal((await instance.balanceOf.call(data.addr_1)).toString(), "20000");
 
     await revertSnapshot(snapshot);
   });
 
   test("closeChannelFast happy path no hashlocks", async t => {
     const snapshot = await takeSnapshot();
-    const data = await getData(11);
-    const data1 = await getSettlingData(11);
+    const data = await getData(1);
+    const data1 = await getSettlingData(1);
 
     await createChannel(instance, data);
 
     await instance.closeChannelFast(
       "0x" + data1.chl_id,
-      "0x" + data1.seq_num,
-      "0x" + data1.bal_0,
-      "0x" + data1.bal_1,
+      data1.seq_num,
+      data1.bal_0,
+      data1.bal_1,
       "0x",
       data1.sig_0_cl,
       data1.sig_1_cl
     );
 
-    t.equal(
-      (await instance.balanceOf.call(data.address_0)).toString(),
-      "24000"
-    );
-    t.equal(
-      (await instance.balanceOf.call(data.address_1)).toString(),
-      "20000"
-    );
+    t.equal((await instance.balanceOf.call(data.addr_0)).toString(), "24000");
+    t.equal((await instance.balanceOf.call(data.addr_1)).toString(), "20000");
 
     await revertSnapshot(snapshot);
   });
@@ -213,14 +212,15 @@ module.exports = async (test, instance) => {
     const data = await getData(1);
     const data1 = await getSettlingData(1);
 
-    await createChannel(instance, data);
+    //await createChannel(instance, data);
+    // already existing
 
     await t.shouldFail(
       instance.closeChannelFast(
         "0x" + data1.chl_id_lt,
-        "0x" + data1.seq_num,
-        "0x" + data1.bal_0,
-        "0x" + data1.bal_1,
+        data1.seq_num,
+        data1.bal_0,
+        data1.bal_1,
         "0x",
         data1.sig_0_cl,
         data1.sig_1_cl
@@ -236,13 +236,14 @@ module.exports = async (test, instance) => {
     const data1 = await getSettlingData(1);
 
     await createChannel(instance, data);
+    //already created
 
     await t.shouldFail(
       instance.closeChannelFast(
         "0x" + data1.chl_id,
-        "0x" + data1.seq_num,
-        "0x" + data1.bal_0,
-        "0x" + data1.bal_1,
+        data1.seq_num,
+        data1.bal_0,
+        data1.bal_1,
         "0x",
         data.sig_start_stl_p,
         data.sig_bogus_msg
@@ -257,14 +258,16 @@ module.exports = async (test, instance) => {
     const data = await getData(1);
     const data1 = await getSettlingData(1);
 
+    //console.log("await createChannel(instance, data)");
     await createChannel(instance, data);
+    //already created
 
     await t.shouldFail(
       instance.closeChannelFast(
         "0x" + data1.chl_id,
-        "0x" + data1.seq_num,
-        "0x" + data.bogus_amount,
-        "0x" + data1.bal_1,
+        data1.seq_num,
+        data.bogus_amount,
+        data1.bal_1,
         "0x",
         data1.sig_0_cl,
         data1.sig_1_cl
