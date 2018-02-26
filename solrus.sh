93d04f622a1cc28f31c22fd34bcd3d857e7b6384
ijanus <emekamicro@gmail.com>
2018-02-26 22:02:34 +0100

    Cleaned up code

--- solrus.sh
+++ solrus.sh
@@ -1,47 +1,33 @@
 #!/bin/sh
 
-XTERM=`which xterm`
-KONSOLE=`which konsole`
+
 NPM=`which npm`
 NODE=`which node`
 
-if [ -z "$XTERM" -o -z "$KONSOLE" -o -z "$NPM" -o -z "$NODE" ]; then
-  echo "Required tools are missing - install either xterm or konsole"
+if [ -z "$NPM" -o -z "$NODE" ]; then
+  echo "Required tools are missing - install either npm or node"
   exit 1
 fi
 
 start_jsonserver() {	
 	cd rinterface/lib
-	if [ -x "$XTERM" ]; then
-		`"$XTERM" -hold -e cargo test -- --nocapture` &
-	else
-		`"$KONSOLE" --noclose -e cargo test -- --nocapture` &
-	fi
-	cd ../../
-	echo "Sleep for 90 seconds to allow Cargo to complete"
-	sleep 90
-}
-
-start_testrpcserver() {	
-	if [ -x "$XTERM" ]; then		
-		`"$XTERM" -hold -e truffle develop` &
-	else
-		`"$KONSOLE" --noclose -e truffle develop` &
-	fi
-	echo "Sleep for 30 seconds to allow test rpc server to launch"
-	sleep 30
+	cargo test -- --nocapture &
+	cd ../../smartContract
+	echo "Sleep for 50 seconds to allow Cargo to complete"
+	sleep 50
 }
 
 test_smartcontract() {	
-	cd smartContract
-	#npm install
+	npm install
+	testrpc &
+	sleep 15
+	rm -rf /build
 	npm run migrate
 	npm run test
 	cd ..
 }
 
 start_jsonserver
-start_testrpcserver
 test_smartcontract
 
 echo "Done O_0"
