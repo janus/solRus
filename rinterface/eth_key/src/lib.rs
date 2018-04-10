72be2b752f66282756a064b3c0292b9e4f19f411
ijanus <emekamicro@gmail.com>
2018-04-10 09:14:29 +0100

    Removed rustc-hex crate

--- rinterface/eth_key/src/lib.rs
+++ rinterface/eth_key/src/lib.rs
@@ -1,6 +1,5 @@
 extern crate ethkey;
 extern crate multihash;
-extern crate rustc_hex;
 
 pub mod eth_hash_and_signature;
 
@@ -9,21 +8,18 @@ pub use self::eth_hash_and_signature::hash_and_sign;
 #[cfg(test)]
 mod tests {
     use super::*;
-    use ethkey::{sign, KeyPair, Secret};
-    use multihash::{encode, to_hex, Hash};
+    use ethkey::{KeyPair, Secret};
     use std::str::FromStr;
 
     fn update_state_signs() -> String {
         let open_secret_0 = "a100df7a048e50ed308ea696dc600215098141cb391e9527329df289f9383f65";
         let keypair_0 = KeyPair::from_secret(Secret::from_str(open_secret_0).unwrap()).unwrap();
-
-        let update_state = to_hex(&"updateState".as_bytes());
-        hash_and_sign(&keypair_0.secret(), &[&update_state])
+        hash_and_sign(&keypair_0.secret(), "updateState".as_bytes())
     }
 
     #[test]
     fn test_hash_and_sign() {
-        let update_state_sign_val = "0xc668d80913a092307a51da6ce1c15cdf1e1d15821c6cdd3fcfa7e176571829b02232381c31db720e5cee2333bc13cb9bd76785841eeb428dfeff371a82def39401";
-        assert_eq!(update_state_signs(), update_state_sign_val);
+        let update_state_signed_val = "0xc668d80913a092307a51da6ce1c15cdf1e1d15821c6cdd3fcfa7e176571829b02232381c31db720e5cee2333bc13cb9bd76785841eeb428dfeff371a82def39401";
+        assert_eq!(update_state_signs(), update_state_signed_val);
     }
 }
