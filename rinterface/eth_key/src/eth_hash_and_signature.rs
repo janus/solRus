bf54595076796c134e1da25cc294907d43046751
ijanus <emekamicro@gmail.com>
2018-04-10 09:15:45 +0100

    Removed string concatenation and from_hex fn

--- rinterface/eth_key/src/eth_hash_and_signature.rs
+++ rinterface/eth_key/src/eth_hash_and_signature.rs
@@ -1,15 +1,9 @@
-use ethkey::Message;
-use ethkey::{sign, KeyPair, Secret};
-use multihash::{encode, to_hex, Hash};
-use rustc_hex::{FromHex, ToHex};
+use ethkey::{sign, Secret, Message};
+use multihash::{encode, Hash};
 
-pub fn hash_and_sign(secret: &Secret, vec_str: &[&str]) -> String {
-    let mut str_val = String::new();
-    for item in vec_str {
-        str_val.push_str(item);
-    }
 
-    let bytes = str_val.from_hex().unwrap();
+pub fn hash_and_sign(secret: &Secret, bytes: &[u8]) -> String {
+
     let fingerprint = encode(Hash::Keccak256, &bytes).unwrap();
     let msg = Message::from_slice(&fingerprint[2..]);
     format!("0x{}", sign(&secret, &msg).unwrap())
